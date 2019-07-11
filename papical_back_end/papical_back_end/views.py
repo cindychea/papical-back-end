9f10f58657705289860528fc37393d7ed72722f7
Jasmin Saromo <jasminsaromo@gmail.com>
2019-07-11 19:12:41 -0400

    Added FriendRequestViewSet and methods.

--- papical_back_end/papical_back_end/views.py
+++ papical_back_end/papical_back_end/views.py
@@ -1,9 +1,13 @@
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from papical_back_end.models import User, Hangout, FreeTime, Invitation
 from taggit_serializer.serializers import TaggitSerializer
 from taggit.models import Tag
-from friendship.models import Friend
-from papical_back_end.serializers import UserSerializer, HangoutSerializer, FreeTimeSerializer, InvitationSerializer, TagSerializer, FriendSerializer
+from friendship.models import Friend, FriendshipRequest
+from papical_back_end.serializers import UserSerializer, HangoutSerializer, FreeTimeSerializer, InvitationSerializer, TagSerializer, FriendSerializer, FriendshipRequestSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
   serializer_class = UserSerializer
@@ -146,8 +150,9 @@ class FriendViewSet(viewsets.ModelViewSet):
   permission_classes = (permissions.IsAuthenticated,)
   
   # def list(self, request):
-  #   # List
-  #   pass
+  #   queryset = Friend.objects.select_related("from_user", "to_user").filter(to_user=request.user).all()
+  #   serializer = FriendSerializer(queryset, many=True)
+  #   return Response(serializer.data)
 
   # def create(self, request):
   #   pass
@@ -163,4 +168,87 @@ class FriendViewSet(viewsets.ModelViewSet):
   #   pass
 
   # def destroy(self, request, pk=None):
-  #   pass
+  #   Friend.objects.remove_friend(request.user, to_user)
+
+  #   return Response({'status': 'Request deleted'}, status=200)
+
+
+class FriendRequestViewSet(viewsets.ModelViewSet):
+  serializer_class = FriendshipRequestSerializer
+  queryset = FriendshipRequest.objects.all()
+  permission_classes = (permissions.IsAuthenticated, )
+
+  def list(self, request):
+    queryset = FriendshipRequest.objects.select_related("from_user", "to_user").filter(to_user=request.user).all()
+    serializer = FriendshipRequestSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+  def create(self, request):
+    # to_user = user_model.objects.get(username=to_username)
+    to_user = "" # get id from request params or url path
+    Friend.objects.add_friend(request.user, to_user)
+    # serializer = FriendshipRequestSerializer(queryset, many=True)
+    return Response({'status': 'Request sent'}, status=200)
+
+  def retrieve(self, request, pk=None):
+    queryset = FriendshipRequest.objects.get(pk=pk)
+    serializer = FriendshipRequestSerializer(queryset, many=False)
+    return Response(serializer.data)
+
+  def update(self, request, pk=None):
+    friendship = FriendshipRequest.objects.get(pk=pk)
+
+    answer = request.data["answer"]
+    
+    # {
+    #   "accepted": friendship.accept,
+    #   "rejected": friendship.reject
+    # }.get(answer)()
+
+    if answer == "accepted":
+      friendship.accept()
+      return Response({'status': 'Request accepted'}, status=200)
+    elif answer == "rejected":
+      friendship.reject()
+      return Response({'status': 'Request rejected'}, status=200)
+
+  def destroy(self, request, pk=None):
+    friendship = FriendshipRequest.objects.get(pk=pk)
+    friendship.cancel()
+    return Response({'status': 'Request deleted'}, status=200)
+
+
+
+
+
+
+
+
+
+
+
+
+  # def list(self, request):
+  #   queryset = Friend.objects.all()
+  #   serializer = FriendSerializer(queryset, many=True)
+  #   return Response(serializer.data)
+
+  # @action(detail=True, methods=['get'])
+  # def list_requests(self, request, pk=None):
+  #   queryset = Friend.objects.unread_requests(request.user)
+  #   serializer = FriendSerializer(queryset, many=True)
+  #   return Response(serializer.data)
+
+  # @action(detail=True, methods=['post'])
+  # def make_request(self, request, pk=None):
+  #   other_user = User.objects.get(pk=1)
+  #   serializer = FriendSerializer(data=request.data)
+
+  #   if serializer.is_valid():
+  #     serializer.save()
+  #     return Response(serializer.data)
+  #   return Response(serializer.errors)
+
+
+
+  # https://github.com/revsys/django-friendship/blob/master/friendship/views.py
