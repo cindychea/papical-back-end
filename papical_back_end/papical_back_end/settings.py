cddf0d05c70884613fdf4909590de848f8df5098
Jasmin Saromo <jasminsaromo@gmail.com>
2019-07-15 15:39:11 -0400

    Added default profile picture.

--- papical_back_end/papical_back_end/settings.py
+++ papical_back_end/papical_back_end/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'papical_back_end.User'
 
--- papical_back_end/papical_back_end/urls.py
+++ papical_back_end/papical_back_end/urls.py
@@ -22,6 +22,9 @@ from rest_framework import routers
 from rest_framework_simplejwt import views as jwt_views
 
 from papical_back_end import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 router = routers.DefaultRouter()
@@ -42,5 +45,8 @@ urlpatterns = [
     path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
