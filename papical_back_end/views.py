a7199bc1b54afe192a8d4f21483116a8ebabbdde
cindychea <cindy_chea@hotmail.com>
2019-07-17 16:41:23 -0400

    Commented out views that are not functional.

--- papical_back_end/views.py
+++ papical_back_end/views.py
@@ -83,22 +83,22 @@ class FreeTimeViewSet(viewsets.ModelViewSet):
   # def get_serializer_context(self):
   #   return {'request': self.request}
 
-  def get_serializer_context(self):
-    """
-    pass request attribute to serializer
-    """
-    context = super(FreeTimeViewSet, self).get_serializer_context()
-    return context
+  # def get_serializer_context(self):
+  #   """
+  #   pass request attribute to serializer
+  #   """
+  #   context = super(FreeTimeViewSet, self).get_serializer_context()
+  #   return context
   
   # def list(self, request):
   #   # List
   #   pass
 
-  def create(self, request):
+  # def create(self, request):
 
-  #   creator =  self.context['request'].user
-  #   serializer.save(creator=creator)
-    pass
+  # #   creator =  self.context['request'].user
+  # #   serializer.save(creator=creator)
+  #   pass
 
   # def retrieve(self, request, pk=None):
   #   # Detail
